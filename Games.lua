795eca436f977031ed1854af7b5cd49d49f2817d
IAmSorrowful <97572520+IAmSorrowful@users.noreply.github.com>
2022-11-08 14:18:22 -0600

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,15 +1,8 @@
 local Games = {
-    [155615604] = {
-        ["Version"] = {297},
-        ["ScriptLink"] = {"https://raw.githubusercontent.com/IAmSorrowful/Admin/main/Admin.lua"},
-        ["Name"] = {"Prison Life"},
-        ["ID"] = {155615604}
-    },
-    [6152116144,11468034852,6361937392] = {
+    ["Project Slayers"] = {
         ["Version"] = {19233},
         ["ScriptLink"] = {"https://raw.githubusercontent.com/IAmSorrowful/Admin/main/Project%20Slayers.lua"},
         ["Name"] = {"Project Slayers"},
         ["ID"] = {6152116144,11468034852,6361937392}
     },
 }
-return Games;
