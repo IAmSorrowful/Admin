a37658869b57ed88376426b8636c4bf62563316f
IAmSorrowful <97572520+IAmSorrowful@users.noreply.github.com>
2022-11-07 23:44:17 -0600

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,11 @@
+local Games = {
+    [155615604] = {
+        ["Version"] = 297;
+        ["ScriptLink"] = "https://raw.githubusercontent.com/IAmSorrowful/Admin/main/Admin.lua";
+    },
+    [5956785391] = {
+        ["Version"] = nil;
+        ["ScriptLink"] = ""
+    },
+};
+return Games;
