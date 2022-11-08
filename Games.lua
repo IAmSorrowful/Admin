fc220b182c27f5ccb86bf4e20e30f56c1b56fef0
IAmSorrowful <97572520+IAmSorrowful@users.noreply.github.com>
2022-11-07 23:54:26 -0600

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,10 +1,10 @@
 local Games = {
     [155615604] = {
-        ["Version"] = 297;
-        ["ScriptLink"] = "https://raw.githubusercontent.com/IAmSorrowful/Admin/main/Admin.lua";
+        ["Version"] = 297,
+        ["ScriptLink"] = "https://raw.githubusercontent.com/IAmSorrowful/Admin/main/Admin.lua"
     },
     [6152116144] = {
-        ["Version"] = nil;
+        ["Version"] = nil,
         ["ScriptLink"] = ""
     },
 };
